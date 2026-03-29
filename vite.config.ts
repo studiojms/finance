c53016bb41f3efdfee1ec9666a802633d9eba0e9
Jefferson Mariano de Souza <158855+myuseremail@users.noreply.github.com>
2026-03-28 21:57:29 -0300

    chore: adjust github pages config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig(({mode}) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
+    // Set base path for GitHub Pages deployment
+    base: process.env.GITHUB_PAGES === 'true' ? '/finance/' : '/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
